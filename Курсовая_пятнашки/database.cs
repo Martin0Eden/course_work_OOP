64394eab6db2660188904b560cfd30fe78d1128f
Martin0Eden <dimka17372@gmail.com>
2023-05-25 20:26:45 +0700

    FINAL+FINAL+BD

--- Курсовая_пятнашки/database.cs
+++ Курсовая_пятнашки/database.cs
@@ -9,9 +9,8 @@ namespace Курсовая_пятнашки
 {
     internal class database
     {
-        /*SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\GameAt15.mdf;Integrated Security=True");*/
 
-        SqlConnection conn = new SqlConnection(@"Data Source=WIN-AG9158AOA4O;Initial Catalog=GameAt15;Integrated Security=True");
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\GameAt15.mdf;Integrated Security=True");
 
         public void open()
         {
